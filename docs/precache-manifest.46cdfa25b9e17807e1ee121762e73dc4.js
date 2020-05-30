7eb98549e9dd50a4bb5cdb9b701d3b2d5ae50570
k.smirnov <k.smirnov@lendohotel.com>
2020-05-30 17:51:45 +0300

    to docs

---
+++ docs/precache-manifest.46cdfa25b9e17807e1ee121762e73dc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ff6a908fdf8e35684fca2c54f4f87b",
+    "url": "/krlls/shops-chart/index.html"
+  },
+  {
+    "revision": "d5140003936235b2b3b1",
+    "url": "/krlls/shops-chart/static/css/main.4d894fd6.chunk.css"
+  },
+  {
+    "revision": "0cd5774a0acc8b22407e",
+    "url": "/krlls/shops-chart/static/js/2.f4bfa36a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/krlls/shops-chart/static/js/2.f4bfa36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5140003936235b2b3b1",
+    "url": "/krlls/shops-chart/static/js/main.86a68a56.chunk.js"
+  },
+  {
+    "revision": "1c41db04980ca9d025af",
+    "url": "/krlls/shops-chart/static/js/runtime-main.7f92340c.js"
+  },
+  {
+    "revision": "c015845a2cb3a71093f4f88949455c44",
+    "url": "/krlls/shops-chart/static/media/icon.c015845a.svg"
+  }
+]);
